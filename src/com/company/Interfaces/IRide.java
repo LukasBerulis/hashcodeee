e111b784665dfef9e7536d457246c7d8527beb34
Romas <rpromas@gmail.com>
2018-03-01 21:14:21 +0200

    sveikas grantai

--- src/com/company/Interfaces/IRide.java
+++ src/com/company/Interfaces/IRide.java
@@ -1,5 +1,12 @@
 package com.company.Interfaces;
 
-public interface IRide {
-
+public interface IRide
+{
+    void IRide(int a,int b, int x, int y,int s, int f);
+    int getA();
+    int getB();
+    int getX();
+    int getY();
+    int getS();
+    int getF();
 }
